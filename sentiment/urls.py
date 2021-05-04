e35fea307eb33ee87a98c34b64de8599b90f0f58
dineshssdn-867 <seemanariani30@gmail.com>
2021-05-04 09:00:21 +0000

    Core functionality done

--- sentiment/urls.py
+++ sentiment/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import HomeView
+from .views import *
 
 app_name='sentiment'
 urlpatterns = [
-    path('', HomeView.as_view(), name='home')
+    path('', HomeView.as_view(), name='home'),
+    path('sentimentform/', FormView.as_view(), name='form'),
+    path('show/', show, name="show")
 ]
--- sentiment/views.py
+++ sentiment/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.http import HttpResponse
 
 
 # Create your views here.
 class HomeView(TemplateView):
     template_name ='sentiment/index.html'
+
+class FormView(TemplateView):
+    template_name ='sentiment/sentiment_form.html'
+
+def show(request):
+    channel_id=request.POST.get('channel_id')
+    publish_date_after=request.POST.get('publish_date_after')
+    publish_date_before=request.POST.get('publish_date_before')
+    print(publish_date_after)
+    print(publish_date_before)
+    return HttpResponse("Http request is: "+request.method)  
+    
+    
+
