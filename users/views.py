325c1ccdbf2c9e80875b69209e02bbbb6ee2333f
dineshssdn-867 <66898317+dineshssdn-867@users.noreply.github.com>
2021-06-15 17:11:01 +0530

    Added verification auth services

--- users/views.py
+++ users/views.py
@@ -44,6 +44,24 @@ class RegisterView(SuccessMessageMixin, CreateView):
 class UserLoginView(LoginView):  # Initializing template for login view
     template_name = 'users/login.html'
 
+    def form_valid(self, form):
+        username = form['username'].value()  # getting the email from form object
+        password = form['password'].value()  # getting the password from form object
+        email = User.objects.filter(username=username).values('email')
+        try:  # some basic validation of e-mail
+            user = auth.sign_in_with_email_and_password(email[0]['email'], password)  # login with e-mail and password
+            user_info = auth.get_account_info(user['idToken'])
+            if user_info['users'][0]['emailVerified']:
+                return super().form_valid(form)
+            else:
+                messages.error(self.request,
+                               'Please verify your email')  # adding the errors in messages list which will be shown in message.html template
+                return HttpResponseRedirect(reverse('users:login'))  # Redirecting to form page if there are any errors
+        except:
+            messages.error(self.request,
+                           'Please check your password and e-mail')  # adding the errors in messages list which will be shown in message.html template
+            return HttpResponseRedirect(reverse('users:login'))  # Redirecting to form page if there are any errors
+
 
 class UserLogoutView(LogoutView):  # Initializing template for logout view
     template_name = 'users/login.html'
