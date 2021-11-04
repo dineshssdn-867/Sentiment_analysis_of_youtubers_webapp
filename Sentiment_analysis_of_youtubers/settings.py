aefcf5b93650a92c6706b8e5b773c4769b3a78eb
dineshssdn-867 <66898317+dineshssdn-867@users.noreply.github.com>
2021-11-04 13:17:37 +0530

    Push

--- Sentiment_analysis_of_youtubers/settings.py
+++ Sentiment_analysis_of_youtubers/settings.py
@@ -74,6 +74,12 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'loaders': [
+                ('django.template.loaders.cached.Loader', [
+                    'django.template.loaders.filesystem.Loader',
+                    'django.template.loaders.app_directories.Loader',
+                ]),
+            ],
         },
     },
 ]
