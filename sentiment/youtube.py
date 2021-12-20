084ead4f57f3f008b272c9ec32f3e19edc1f0f01
dineshssdn-867 <66898317+dineshssdn-867@users.noreply.github.com>
2021-12-20 09:18:38 +0530

    Small change in comments fetch

--- sentiment/youtube.py
+++ sentiment/youtube.py
@@ -55,7 +55,7 @@ def get_subtitles(video_ids):
 
 def get_youtube_comment_data(video_id: AnyStr) -> AnyStr:
     x = requests.get(
-        'https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId=' + video_id + '&key=AIzaSyDnIqoMPASXgKPkzxlcy4krIPOHtJOJ998&maxResults=50')  # getting the data of channel/video
+        'https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId=' + video_id + '&key=AIzaSyDnIqoMPASXgKPkzxlcy4krIPOHtJOJ998&maxResults=100')  # getting the data of channel/video
     text = ''
     if 200 <= x.status_code <= 399:  # some basic validations
         values = json.loads(x.text)  # we will parse the text to json and json to dictionary
